e2e16e6f195fab7c919e9a9c38a53e6c983a5bb4
syyyym <ameliasophiehayes@gmail.com>
2019-09-24 17:00:55 +0000

    Add a bootstrap search input group

--- app/components/Global.js
+++ app/components/Global.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Button, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+
+    search() {
+        console.log("search");
+    }
+
     render() {
         return (
-            <div>The Global App Component</div>
+            <div>
+                <h2>Book Explorer!</h2>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            placeholder="Search for a Book"
+                        />
+                        <Button type="submit" onClick={() => this.search()}>Search</Button>
+                    </InputGroup>
+                </FormGroup>
+            </div>
         )
     }
 }
+
+
 
 export default Global;
