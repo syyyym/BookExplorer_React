521258d17edbc89c551ba2f685234b9415d89c6c
syyyym <ameliasophiehayes@gmail.com>
2019-10-01 02:33:48 +0000

    add data from the google books API and css styling

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 
 class Gallery extends Component {
   render() {
+    let alternate = 'https://cdn0.iconfinder.com/data/icons/thin-photography/57/thin-367_photo_image_wall_unavailable_missing-512.png';
+
     return (
       <div>
         {
           this.props.items.map((item, index) => {
-            let { title } = item.volumeInfo;
+            let { title, imageLinks, infoLink } = item.volumeInfo;
             return (
-              <div key={index}>{title}</div>
+              <a
+                key={index}
+                className="book"
+                href={infoLink}
+                target="_blank"
+              >
+                <img
+                  src={imageLinks !== undefined ? imageLinks.thumbnail : alternate}
+                  alt="book image"
+                  className="book-img"
+                />
+                <div className="book-text">
+                  {title}
+                </div>
+              </a>
             )
           })
         }
